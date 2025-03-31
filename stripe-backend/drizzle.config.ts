bb574f668702be7986016db0ce58a20a1be482e6
seenu30 <srinivas.reddy.abbavaram@gmail.com>
2025-03-31 12:14:02 +0530

    Fixed DATABASE_URL in drizzle config

--- stripe-backend/drizzle.config.ts
+++ stripe-backend/drizzle.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   out: "./migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: `postgresql://neondb_owner:npg_QIEYmNSAe2J5@ep-raspy-tree-a5q0lbt8-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require`,
+    url: process.env.DATABASE_URL,
   },
 });
